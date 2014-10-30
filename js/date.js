5983639f11bbe1033a73955aecc48dd01dfee6f2
Jerissa Lumansoc <JerissaLumansoc@gmail.com>
2014-10-29 23:49:59 -0700

    no message

--- js/date.js
+++ js/date.js
@@ -2,11 +2,44 @@
 $(function(){
 	$(".template").hide();
 
-	$("#plan-my-date").submit(function() {
-		var dateLocation = $("input[name=location]").val();
-		var dateEvent = $("input[name=event]").val();
-		var dateEat = $("input[name=eat]").val();
+	// $("input[type=search]").keyup(function() {
+	// 	getDates();
+	// });
 
-		alert(dateLocation + " " + dateEvent + " " +dateEat);
+	$("#plan-my-date").submit(function() {
+		getDates();
+		return false; // keeps on same page
 	});
 });
+
+function getDates() {
+	var dateLocation = $("input[name=location]").val();
+	var dateEvent = $("input[name=event]").val();
+	var dateEat = $("input[name=eat]").val();
+
+	// alert(dateLocation + ", " + dateEvent + ", " +dateEat);
+
+	getEvents();
+}
+
+function getEvents(dateLocation) {
+	$.ajax({
+        type: "POST",
+        url: "http://api.eventful.com/json/events/search",
+        data: {app_key: "K7b4cBjVXBTFm2wW", location: "seattle"},
+        contentType: "application/json; charset=utf-8",
+        dataType: "jsonp",
+        success: function (data) {
+        	// $(".loader").hide();
+        	// alert("success");
+        },
+        error: function (xhr, status, error) {
+            // $(".loader").hide();
+            // alert("error");
+        }
+    });
+}
+
+function formatEvents(data) {
+
+}
