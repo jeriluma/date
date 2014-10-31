180233f3162e90712e56d903d8246544b25fdc11
Jerissa Lumansoc <JerissaLumansoc@gmail.com>
2014-10-31 09:58:48 -0700

    no message

--- js/date.js
+++ js/date.js
@@ -56,44 +56,8 @@ function getEats() {
     var Token = "s0dgcSrQLI3jH1_nLygF11r-L0mmiLod";
     var Token_Secret = "iDCdK6XXxwLQ0_CWVn_pD1xknZk";
 
-	$.ajax({
-        type: "post",
-        url: "http://api.yelp.com/v2/search?_=" + Math.round((new Date()).getTime() / 1000),
-        data: {
-            oauth_consumer_key: 
-        	oauth_consumer_key: Consumer_Key,
-            oauth_nonce: makeid(),
-        	oauth_signature_method: "HMAC-SHA1",
-        	oauth_signature: Consumer_Secret + "%26" + Token_Secret,
-        	oauth_timestamp: Math.round((new Date()).getTime() / 1000),
-        	oauth_token: Token,
 
-            term: "food"
-        },
-        contentType: "application/json; charset=utf-8",
-        dataType: "jsonp",
-        success: function (data) {
-        	// $(".loader").hide();
-        	// alert("success");
-        	formatEats();
-        },
-        error: function (xhr, status, error) {
-            // $(".loader").hide();
-            // alert("error");
-        }
-    });
 }
-
-function makeid() {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for(var i=0; i < 5; i++ )
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-    return text;
-}
-
 
 function formatEvents() {
 
